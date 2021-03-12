8cd989b26c48afa455eab78dc2fbb3990a152740
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2021-03-12 16:03:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import TeamDetail from './Components/Card/TeamDetail';
+import Home from './Components/Home/Home';
+
+function App() {
+	return (
+		<div className="App">
+			<Router>
+				<div>
+					{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/teams/:id">
+							<TeamDetail />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import TeamCard from '../Card/TeamCard';
+import Header from '../Header/Header';
+
+const Home = () => {
+	const [ teams, setTeams ] = useState([]);
+
+	const url = `https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League`;
+
+	useEffect(() => {
+		fetch(url).then((res) => res.json()).then((data) => {
+			setTeams(data.teams);
+			console.log(data.teams);
+		});
+	}, []);
+
+	// function getAllTeams
+
+	return (
+		<div>
+			<Header title="TEAM TRACKER" />
+
+			<div style={{ backgroundColor: 'darkslateblue', padding: '20px' }}>
+				<div className="container">
+					<div className="row">{teams.map((team) => <TeamCard team={team} />)}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
